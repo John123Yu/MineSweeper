bfd8bd655c924dec32d61ab200df21b79d80247e
John Yu <john.yu@reisystems.com>
2019-03-24 10:00:51 -0400

    Updates

---
+++ precache-manifest.e407cb3d39ea7e1103af61c679c4bf9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ccb8ed1efdda955d2877",
+    "url": "/MineSweeper/static/css/main.28151bff.chunk.css"
+  },
+  {
+    "revision": "ccb8ed1efdda955d2877",
+    "url": "/MineSweeper/static/js/main.2dff59b5.chunk.js"
+  },
+  {
+    "revision": "8e66a548f95acc978c1b",
+    "url": "/MineSweeper/static/js/runtime~main.f1c351cb.js"
+  },
+  {
+    "revision": "5e4317895bb404792711",
+    "url": "/MineSweeper/static/js/2.346e71ad.chunk.js"
+  },
+  {
+    "revision": "5ee244e32c0b3ef88196a10050b0df98",
+    "url": "/MineSweeper/index.html"
+  }
+];
